48a49fba10d4a1003c3074d69bec1d695bb1d860
matias astudillo <astudillomatias89@gmail.com>
2024-02-12 16:41:00 -0300

    Problema_resuelto

--- src/app/agregar-tarea/agregar-tarea.component.ts
+++ src/app/agregar-tarea/agregar-tarea.component.ts
@@ -13,11 +13,16 @@ export class AgregarTareaComponent {
   constructor(private tareasService: TareasService) {}
 
   agregarTarea() {
-    console.log("hola")
-    console.log('Datos a enviar al backend:', this.modelo);
-    this.tareasService.agregarTarea(this.modelo).subscribe(() => {
-      console.log('Datos a enviar al backend:', this.modelo);
+    if (this.modelo.completado == null) {
+      
+      this.modelo.completado = false;
+
+    }
+    
+    this.tareasService.agregarTarea(this.modelo).subscribe(() => { 
+      
       this.modelo = {} as Modelo; // Reinicia el modelo después de agregar la tarea
+
     });
   }
 }
